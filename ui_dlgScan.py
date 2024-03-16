da49d62d6b63f39c47b46711bdec39b760c49f0f
Stanley <stanley_shao@126.com>
2024-03-16 19:54:28 +0800

    Downgrade to Qt 6.4

--- ui_dlgScan.py
+++ ui_dlgScan.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'dlgScan.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.6.1
+## Created by: Qt User Interface Compiler version 6.4.0
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
